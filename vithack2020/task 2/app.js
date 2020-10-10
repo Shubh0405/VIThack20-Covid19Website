0f50daf2585fd910fd25d2706b64435dbb58fa6d
Shubh Gupta <shubhngupta04@gmail.com>
2020-10-10 17:40:16 +0530

    tables added in task 3

--- vithack2020/task 2/app.js
+++ vithack2020/task 2/app.js
@@ -12,17 +12,31 @@ async function renderUsers() {
     let users = await getUsers();
     let html = '';
     users.data.notifications.forEach(user => {
-        let htmlSegment = `<div class="user">
-                            <a href="${user.link}">${user.title}</a>
-                        </div>`;
+        // let htmlSegment = `<div class="user">
+        //                     <a href="${user.link}">${user.title}</a>
+        //                 </div>`;
+
+        let htmlSegment = `<tr><td> <a href="${user.link}">${user.title}</a> </td></tr>`;
 
         html += htmlSegment;
     });
 
-    let container = document.querySelector('.container');
+    let container = document.querySelector('.notifications_section');
     container.innerHTML = html;
 
 
 }
 
 renderUsers();
+
+var $el = $(".table-responsive");
+function anim() {
+  var st = $el.scrollTop();
+  var sb = $el.prop("scrollHeight")-$el.innerHeight();
+  $el.animate({scrollTop: st<sb/2 ? sb : 0}, 50000, anim);
+}
+function stop(){
+  $el.stop();
+}
+anim();
+$el.hover(stop, anim);
