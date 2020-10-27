eba2c6c44219d4c0bf776e160489d5315b6a471a
Shubh Gupta <shubhngupta04@gmail.com>
2020-10-27 06:41:21 +0530

    collectstatic added

--- vit_hack/settings.py
+++ vit_hack/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '%qvhya@-h0ya42c=gzkl_mc#)q!3rugy1fq$wbj3vn&cy61q_x'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['covinfo19.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -119,7 +119,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [STATIC_DIR,]
